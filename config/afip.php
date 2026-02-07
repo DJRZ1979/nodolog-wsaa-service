c6baf7ae8803fe4f68d5b23ccce742cc9e2c800a
DJRZ1979 <76251589+DJRZ1979@users.noreply.github.com>
2026-02-07 09:40:29 -0300

    Update afip.php
    change folder log by tmp

--- config/afip.php
+++ config/afip.php
@@ -4,16 +4,24 @@ return [
     'cuit' => '27278968389',
     'env'  => 'homo',
 
+    // Certificados (estas rutas están perfectas)
     'cert' => __DIR__ . '/../certs/cert.pem',
     'key'  => __DIR__ . '/../certs/cert.key',
 
+    // WSDL
     'wsaa_wsdl_homo' => 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms?wsdl',
     'wsaa_wsdl_prod' => 'https://wsaa.afip.gov.ar/ws/services/LoginCms?wsdl',
 
+    // Servicio por defecto
     'service_default' => 'wsfe',
-    'log_path'        => __DIR__ . '/../logs',
 
-    'auth_token'      => 'b7f2e1c9a4d8f0e3b1c7d9a2f4e6b8c1',
-    'ta_margin'       => 120,
-    'ta_file'         => __DIR__ . '/../logs/ta.xml',
+    // LOGS y TA en /tmp (Render permite escritura aquí)
+    'log_path' => '/tmp',
+    'ta_file'  => '/tmp/ta.xml',
+
+    // Seguridad
+    'auth_token' => 'b7f2e1c9a4d8f0e3b1c7d9a2f4e6b8c1',
+
+    // Margen de validez del TA
+    'ta_margin' => 120,
 ];
